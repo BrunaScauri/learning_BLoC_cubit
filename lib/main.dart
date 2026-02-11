2497082b7d1cee8ffa2f0eba4e1fa3720dc14300
brunascauri <brunascauri@hotmail.com>
2026-02-11 15:29:51 -0300

    fix: hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+        debugShowCheckedModeBanner: false,
         home: HomeScreen(),
       ),
     );
